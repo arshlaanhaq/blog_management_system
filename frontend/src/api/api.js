d62a0ca4c789475b28e000e388a3bb713f8acb5f
Arshlaan haque <arshlaanhaq005@gmail.com>
2025-01-30 21:45:33 +0530

    change

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios  from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://blog-management-system-1itu.onrender.com/api',
 })
 API.interceptors.request.use((req) => {
         const token = localStorage.getItem('token')
